a4ae4ea263826a62a53e7696e6ffcce45c29a3c1
mbzk <bilalzafar2323@gmail.com>
2020-06-15 23:49:36 +0500

    js code pushed

---
+++ app.js
@@ -0,0 +1,89 @@
+// CHAPTER # 01 : ALERTS
+
+// Task # 01
+// alert("hello world!")
+
+
+// Task # 02
+
+// alert("Error! please enter a valid password")
+
+
+// Task # 03
+
+// alert("welcome to JS land" + "Happy coding")
+
+
+// Task # 04
+
+// alert("welcome to JS land")
+// alert("Happy Coding")
+
+
+// Task # 05
+
+// alert("")
+
+
+// CHAPTER # 02 : VARIABLE
+
+
+// Task # 01
+
+// a = "username"
+
+
+// Task # 02
+
+// MyName = "MUHAMMAD BILAL ZAFAR KHAN"
+
+
+// TASK # 03
+
+// var message;
+// message = "hello world!"
+// alert(message)
+
+
+// Task # 04
+
+// var StudentName = "John Doe"
+// alert(StudentName)
+// var StudentAge = "15"
+// alert(StudentAge + "years old")
+// var StudentCertificate = "Web and App dev"
+// alert("certificate" + " " + "in" + " " + StudentCertificate)
+
+
+// Task # 05
+
+
+// Task # 06
+
+// var email;
+// email = "bilalzafar2323@gmail.com"
+// alert("my email adddress is " + " " + email)
+
+
+// Task # 07
+
+// var book;
+// book = “A smarter way to learn JavaScript”
+// alert("i m tyin to learn from the Book"  + book )
+
+
+// Task # 08
+
+// document.write("yes I can run JS through my JS")
+
+// Task # 09
+
+//  var a = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+CHAPTER # 03
+ 
+
+Task # 01
+
+var a
